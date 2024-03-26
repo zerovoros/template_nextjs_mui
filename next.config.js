ce183258f2090ec4ff3b135762cb05476d108eb5
Bruno Goncalves <brunnofpg@hotmail.com>
2024-03-26 20:12:36 -0300

    feat: next bundler analyzer + mui better build

--- next.config.js
+++ next.config.js
@@ -1,12 +1,63 @@
-/** @type {import('next').NextConfig} */
-
 const path = require('path');
 
+const withBundleAnalyzer = require('@next/bundle-analyzer')({
+  enabled: process.env.ANALYZE === 'true',
+});
+
+const advancedHeaders = [
+  {
+    key: 'X-DNS-Prefetch-Control',
+    value: 'on',
+  },
+  {
+    key: 'Strict-Transport-Security',
+    value: 'max-age=63072000; includeSubDomains; preload',
+  },
+  {
+    key: 'X-XSS-Protection',
+    value: '1; mode=block',
+  },
+  {
+    key: 'X-Frame-Options',
+    value: 'SAMEORIGIN',
+  },
+  {
+    key: 'X-Content-Type-Options',
+    value: 'nosniff',
+  },
+  {
+    key: 'Referrer-Policy',
+    value: 'origin-when-cross-origin',
+  },
+];
+
 const nextConfig = {
+  modularizeImports: {
+    '@mui/material/?(((\\w*)?/?)*)': {
+      transform: '@mui/material/{{ matches.[1] }}/{{member}}',
+    },
+    '@mui/icons-material/?(((\\w*)?/?)*)': {
+      transform: '@mui/icons-material/{{ matches.[1] }}/{{member}}',
+    },
+  },
   sassOptions: {
     includePaths: [path.join(__dirname, 'styles')],
   },
   swcMinify: true,
+  transpilePackages: [
+    '@mui/system',
+    '@mui/material',
+    '@mui/material-nextjs',
+    '@mui/icons-material',
+  ],
+  async headers() {
+    return [
+      {
+        source: '/:path*',
+        headers: advancedHeaders,
+      },
+    ];
+  },
 };
 
-module.exports = nextConfig;
+module.exports = withBundleAnalyzer(nextConfig);
